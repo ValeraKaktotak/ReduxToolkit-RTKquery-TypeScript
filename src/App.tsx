21bf7be000e81efe3db513d723d8443e99568ab1
ValeraKaktotak <valerakaktotak@gmail.com>
2023-07-12 10:29:17 +0300

    created RTK store

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useAppSelector } from 'hooks/redux';
+import React from 'react';
+
+const App = () => {
+  
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store/store.ts
@@ -0,0 +1,14 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from 'store/reducers/UserSlice'
+
+const rootReducer = combineReducers({
+  userReducer
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
